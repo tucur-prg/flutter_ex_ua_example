a437dcd5f68f1d6d77f12b5e514b6ad46af0c965
tucur-prg <tucur.prg+github@gmail.com>
2024-09-13 14:11:25 +0900

    nativeコードの呼び出し実装

--- android/app/src/main/kotlin/com/example/flutter_ex_ua_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_ex_ua_example/MainActivity.kt
@@ -2,4 +2,22 @@ package com.example.flutter_ex_ua_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "Channel"
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "hello" ->
+                    result.success("Hello from Kotlin!")
+
+                else ->
+                    result.notImplemented()
+            }
+        }
+
+    }
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,20 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+
+    let methodChannel = FlutterMethodChannel(name: "Channel", binaryMessenger: controller as! FlutterBinaryMessenger)
+    methodChannel.setMethodCallHandler({
+        (call:FlutterMethodCall, result:FlutterResult) -> Void in
+        switch call.method {
+        case "hello" :
+            result("Hello from Swift!")
+        default :
+            result(FlutterMethodNotImplemented)
+        }
+    })
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
